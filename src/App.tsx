31754197ce9969f3cd12945275360f1eca10c08c
tdc <tdecourson@gmail.com>
2025-11-28 19:30:43 +0100

    Fix responsie

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./router";
-import { I18nextProvider } from "react-i18next";
+import {BrowserRouter} from "react-router-dom";
+import {AppRoutes} from "./router";
+import {I18nextProvider} from "react-i18next";
 import i18n from "./i18n";
+import ScrollToTop from "./ScrollToTop.tsx";
+
+
 
 
 function App() {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={__BASE_PATH__}>
-        <AppRoutes />
-      </BrowserRouter>
-    </I18nextProvider>
-  );
+    return (
+        <I18nextProvider i18n={i18n}>
+
+            <BrowserRouter basename={__BASE_PATH__}>
+                <ScrollToTop />
+                <AppRoutes/>
+            </BrowserRouter>
+        </I18nextProvider>
+    );
 }
 
 export default App;
